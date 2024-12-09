bfcf80beab60081f4972facf24bc24941cfb822c
nitmprince <111576489+nitmprince@users.noreply.github.com>
2024-12-09 11:46:00 +0000

    variablepractice

---
+++ basic/vari.js
@@ -0,0 +1,12 @@
+const accountId = 1234;
+let accountEmail ="nitmprince@gmail.com";
+var accountPassword ="avc@123";
+accountEmail ="2nitmprince@gmail.com";
+var accountPassword ="2avc@123";
+//var accountPassword ="sfdasfda@123";
+accountCity = "jaipur";
+//accountId = 7786;
+//accountId = 7786;
+//console.log(accountId);
+
+console.table([accountEmail,accountPassword,accountCity]);
